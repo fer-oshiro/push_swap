67cc8fb21a722c36b810677f5397303a4ffc95a8
Ana Beatrice Schulz Staut <aschulz-@c1r2p2.42sp.org.br>
2026-07-09 16:28:49 -0300

    eumeodeio

--- disorder_metric.c
+++ disorder_metric.c
@@ -1,34 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   disorder_metric.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aschulz- <aschulz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/30 12:27:33 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/07/08 20:52:01 by aschulz-         ###   ########.fr       */
+/*   Created: 2026/07/08 15:40:46 by aschulz-          #+#    #+#             */
+/*   Updated: 2026/07/09 16:19:38 by aschulz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
-void	print_content(void *content)
+double compute_disorder(t_push_swap *data)
 {
-	int num = (int)(long)content;
-	ft_printf(1, "%d\n", num);
-}
+	int mistakes;
+	int	total_pairs;
+	 *i;
+	int j;
 
-int	main(int argc, char **argv)
-{
-	t_push_swap	data;
-
-	handle_initialization_data(&data);
-	if (argc <= 1)
-		return (0);
-	if (!parse_flag(argc, argv, &data))
-		return (0);
-
-	ft_lstiter(data.stack_a->start, &print_content);
-	return (0);
+	mistakes = 0;
+	total_pairs = 0;
+	i = 0;
+	while(i < data->total - 1)
+	{
+		j = i + 1;
+		while(j < data->total)
+		{
+			total_pairs++;
+			if (data->stack_a->content-> data->stack_a->len)
+				mistakes++;
+			j++;
+		}
+		i++;
+	}
+	if (total_pairs == 0)
+		return (0.0);
+	return ((double)mistakes/ total_pairs);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aschulz- <aschulz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 12:29:04 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/07/08 15:10:01 by aschulz-         ###   ########.fr       */
+/*   Updated: 2026/07/09 15:18:35 by aschulz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,17 @@ typedef enum e_bool
 	TRUE
 }	t_bool;
 
+typedef struct s_node
+{
+	void			*content;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
 typedef struct s_stack
 {
-	t_list	*start;
-	t_list	*last;
+	t_node	*start;
+	t_node	*last;
 	int		len;
 }	t_stack;
 
@@ -43,6 +50,7 @@ typedef struct s_push_swap
 	t_stack	*stack_b;
 	t_strategy strategy;
 	t_bool	bench;
+	double	disorder;
 	int		total;
 } t_push_swap;
 
@@ -52,7 +60,7 @@ t_bool	is_valid_int(char *token, t_push_swap *data);
 t_bool	is_strategy_flag(char *tokien, t_push_swap *data);
 long	ft_atol(char *token);
 t_bool	handle_number_token(char *token, t_push_swap *data);
-
+double	compute_disorder(t_push_swap *data);
 void	sort_strategy(char *token, t_push_swap *data);
 
 #endif
