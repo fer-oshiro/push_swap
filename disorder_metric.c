e3588b11c5043e99aec9f05dd03020be343822c1
Fernanda Oshiro <fsayuri-@student.42sp.org.br>
2026-07-10 17:19:18 -0300

    fix norminette

--- disorder_metric.c
+++ disorder_metric.c
@@ -3,44 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   disorder_metric.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aschulz- <aschulz-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 15:40:46 by aschulz-          #+#    #+#             */
-/*   Updated: 2026/07/10 12:05:48 by aschulz-         ###   ########.fr       */
+/*   Updated: 2026/07/10 17:19:11 by fsayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-double compute_disorder(t_push_swap *data)
+static double	total_pairs(t_push_swap *data)
 {
-	int 	mistakes;
-	int 	total_pairs;
+	return (data->stack_a->size * (data->stack_a->size - 1) / 2);
+}
+
+double	compute_disorder(t_push_swap *data)
+{
+	int		mistakes;
 	t_node	*i_node;
 	t_node	*j_node;
 	int		i;
 	int		j;
 
-	total_pairs = 0;
+	if (data->stack_a->size <= 1)
+		return (0.0);
 	mistakes = 0;
 	i = 0;
 	i_node = data->stack_a->start;
-	while(i < data->stack_a->size)
+	while (i < data->stack_a->size)
 	{
 		j = i + 1;
 		j_node = i_node->next;
-		while(j < data->stack_a->size)
+		while (j++ < data->stack_a->size)
 		{
 			if (i_node->content > j_node->content)
 				mistakes++;
-			total_pairs++;
 			j_node = j_node->next;
-			j++;
 		}
 		i_node = i_node->next;
 		i++;
 	}
-	return ((double)mistakes / total_pairs);
-	
+	return ((double)mistakes / ((double)total_pairs(data)));
 }
-
