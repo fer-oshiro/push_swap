ada9c855d83fc021dfa36f76e42192972dfe364d
Fernanda Oshiro <fsayuri-@student.42sp.org.br>
2026-07-10 10:21:15 -0300

    total -> size

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 12:27:33 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/07/10 10:10:33 by fsayuri-         ###   ########.fr       */
+/*   Updated: 2026/07/10 10:19:41 by fsayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	print_content(void *content)
+void	print_content(int content)
 {
 	ft_printf(1, "%d\n", content);
 }
@@ -20,6 +20,7 @@ void	print_content(void *content)
 int	main(int argc, char **argv)
 {
 	t_push_swap	data;
+	int disorder;
 
 	handle_initialization_data(&data);
 	if (argc <= 1)
@@ -27,5 +28,7 @@ int	main(int argc, char **argv)
 	if (!parse_flag(argv, &data))
 		return (0);
 	dlst_iter(*data.stack_a, &print_content);
+	disorder = compute_disorder(&data);
+	ft_printf(1, "%f", disorder);
 	return (0);
 }
