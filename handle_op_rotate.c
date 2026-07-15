04cbb2049b6df6424405f9e7617e3e6179172ab8
Fernanda Oshiro <fsayuri-@student.42sp.org.br>
2026-07-15 11:51:35 -0300

    add op

--- handle_op_rotate.c
+++ handle_op_rotate.c
@@ -6,7 +6,7 @@
 /*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 10:15:20 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/07/15 11:27:22 by fsayuri-         ###   ########.fr       */
+/*   Updated: 2026/07/15 11:51:21 by fsayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static void	dlst_rotate(t_stack *stack)
 {
-    if (!stack || stack->size < 2)
-        return ;
-    stack->start = stack->start->next;
+	if (!stack || stack->size < 2)
+		return ;
+	stack->start = stack->start->next;
 }
 
 void	op_rotate(t_push_swap *data, char rotate_type)
@@ -25,24 +25,24 @@ void	op_rotate(t_push_swap *data, char rotate_type)
 	{
 		dlst_rotate(data->stack_a);
 		ft_printf(1, "ra");
-		return;
+		return ;
 	}
 	if (rotate_type == 'b')
 	{
 		dlst_rotate(data->stack_b);
 		ft_printf(1, "rb");
-		return;
+		return ;
 	}
 	dlst_rotate(data->stack_a);
 	dlst_rotate(data->stack_b);
 	ft_printf(1, "rr");
 }
 
-static void    dlst_reverse_rotate(t_stack *stack)
+static void	dlst_reverse_rotate(t_stack *stack)
 {
-    if (!stack || stack->size < 2)
-        return ;
-    stack->start = stack->start->prev;
+	if (!stack || stack->size < 2)
+		return ;
+	stack->start = stack->start->prev;
 }
 
 void	op_rotate_reverse(t_push_swap *data, char rotate_type)
@@ -51,13 +51,13 @@ void	op_rotate_reverse(t_push_swap *data, char rotate_type)
 	{
 		dlst_reverse_rotate(data->stack_a);
 		ft_printf(1, "rra");
-		return;
+		return ;
 	}
 	if (rotate_type == 'b')
 	{
 		dlst_reverse_rotate(data->stack_b);
 		ft_printf(1, "rrb");
-		return;
+		return ;
 	}
 	dlst_reverse_rotate(data->stack_a);
 	dlst_reverse_rotate(data->stack_b);
--- handle_op_swap.c
+++ handle_op_swap.c
@@ -1,23 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_swap.c                                      :+:      :+:    :+:   */
+/*   handle_op_swap.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/09 10:13:51 by fsayuri-          #+#    #+#             */
-/*   Updated: 2026/07/09 10:15:39 by fsayuri-         ###   ########.fr       */
+/*   Created: 2026/07/15 11:49:48 by fsayuri-          #+#    #+#             */
+/*   Updated: 2026/07/15 11:50:27 by fsayuri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	handle_swap(t_stack *stack)
+static void	dlst_swap(t_stack *stack)
 {
-	// TODO
+	t_node	*first;
+	t_node	*second;
+	int		temp_value;
+	int		temp_index;
+
+	if (!stack || stack->size < 2)
+		return ;
+	first = stack->start;
+	second = first->next;
+	temp_value = first->content;
+	first->content = second->content;
+	second->content = temp_value;
+	temp_index = first->index;
+	first->index = second->index;
+	second->index = temp_index;
 }
 
-void	handle_swap_a_and_b(t_push_swap *data)
+void	op_swap(t_push_swap *data, char rotate_type)
 {
-	// TODO
+	if (rotate_type == 'a')
+	{
+		dlst_swap(data->stack_a);
+		ft_printf(1, "sa");
+		return ;
+	}
+	if (rotate_type == 'b')
+	{
+		dlst_swap(data->stack_b);
+		ft_printf(1, "sb");
+		return ;
+	}
+	dlst_swap(data->stack_a);
+	dlst_swap(data->stack_b);
+	ft_printf(1, "ss");
 }
