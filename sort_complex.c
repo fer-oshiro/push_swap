7592e6dbd962b9b3641ffbd386f480ad38afa06e
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2026-07-29 21:27:23 -0300

    sei la ;-;

---
+++ sort_complex.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_complex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/15 11:49:48 by fsayuri-          #+#    #+#             */
+/*   Updated: 2026/07/15 11:50:27 by fsayuri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	get_max_bits(int max_index);
+static void	radix_pass(t_push_swap *data, int bit);
+
+void	sort_complex(t_push_swap *data)
+{
+
+}
