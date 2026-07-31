c05b812dc94df89075ddd28032ba1182f6f0caa5
Fernanda Oshiro <fsayuri-@student.42sp.org.br>
2026-07-31 10:49:56 -0300

    simples pronto :D

---
+++ sort_simples.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_simples.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/30 17:39:11 by fsayuri-          #+#    #+#             */
+/*   Updated: 2026/07/30 17:39:50 by fsayuri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_simple(t_push_swap *data)
+{
+	int		curr_rotation_cost;
+	t_node	*curr_min_node;
+
+	while (data->stack_a->size)
+	{
+		curr_min_node = stack_min(data->stack_a);
+		curr_rotation_cost = rotation_cost(data->stack_a, curr_min_node);
+		while (curr_rotation_cost > 0)
+		{
+			op_ra(data);
+			curr_rotation_cost--;
+		}
+		while (curr_rotation_cost < 0)
+		{
+			op_rra(data);
+			curr_rotation_cost++;
+		}
+		op_pb(data);
+	}
+	while (data->stack_b->size) {
+		op_pa(data);
+	}
+}
